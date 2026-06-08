df2d5b1e2bb4e74fc979e8a4331d88f5c9dbf150
Tarik Moody <tarikjmoody@gmail.com>
2026-06-08 11:25:46 -0500

    feat(poller): poll orchestrator wiring fetch -> diff -> enqueue (MOO-41)

---
+++ agent/poller/index.js
@@ -0,0 +1,11 @@
+export { detectionKey } from './keys.js';
+export { diffNewItems } from './diff.js';
+export {
+  addDaysIso,
+  buildEventsQuery,
+  createLegistarClient,
+  mapEvent,
+  mapEventItem,
+  toDetectedItem,
+} from './legistar.js';
+export { runPoll } from './poll.js';
